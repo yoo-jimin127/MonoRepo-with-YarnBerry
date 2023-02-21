371bb429346ff44973993a5944491cc8e669c1b5
yoo-jimin127 <dbwlals9936@naver.com>
2023-02-21 19:17:50 +0900

    add : monorepo component import

--- package/front-mono/src/App.tsx
+++ package/front-mono/src/App.tsx
@@ -1,21 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import str from '@monorepo/common'
 
 function App() {
   const [count, setCount] = useState(0)
+  console.log(str)
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{str}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
